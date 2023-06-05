f138c2563b0a8fe323905a3a1b86d8abbb5203ec
airsorna <83644142+airsorna@users.noreply.github.com>
2023-06-05 20:59:10 +0200

    5 June 2023
    5 June push

--- IndeX.js
+++ IndeX.js
@@ -22,3 +22,17 @@ for (var i = 0; i < navigation.length; i++) {
     }
     navigation[i].appendChild(list);
 }
+
+window.onscroll = function() {myFunction()};
+
+var nav = document.getElementById("nav");
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
